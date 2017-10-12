0ac27efbb51de236885bae403e9178bcd6bfd88e
Austin Brovick <austinbrovick@d-173-250-155-157.dhcp4.washington.edu>
2017-10-11 20:01:07 -0700

    Initial Commit

---
+++ CalcArrayPoints/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CalcArrayPoints
+//
+//  Created by Austin Brovick  on 10/11/17.
+//  Copyright © 2017 Austin Brovick . All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
